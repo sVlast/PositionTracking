4cbc35e5eed3f7978851cd0849466f97f9a6f697
dinojelic4 <dinojelic4@gmail.com>
2021-05-18 17:18:33 +0200

    #9  Implemented SignIn and SignOut

--- App/PositionTracking/Controllers/AccountController.cs
+++ App/PositionTracking/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PositionTracking.Models;
 
@@ -12,6 +13,15 @@ namespace PositionTracking.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+
+        {
+            _signInManager = signInManager;
+        }
+       
+
         [AllowAnonymous]
         [HttpGet]
         public IActionResult SignUp(string returnUrl)
@@ -37,13 +47,38 @@ namespace PositionTracking.Controllers
             return View(new SignInModel() { ReturnUrl = returnUrl });
         }
 
-
+        
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         [HttpPost]
         public async Task<IActionResult> SignIn(SignInModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // Checking if url is local prevents redirect attacks. https://docs.microsoft.com/en-us/aspnet/core/security/preventing-open-redirects?view=aspnetcore-2.2
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        return Redirect(model.ReturnUrl);
+                    else
+                        return RedirectToAction("Projects", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("CustomError", "Invalid email or password.");
+                }
+            }
+
             return View(model);
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignOut()
+        {
+           await _signInManager.SignOutAsync();
+            return RedirectToAction("SignIn");
         }
 
 
