6ac7fa41feb57e3fbc13fd9ded67f554d51717fd
dinojelic4 <dinojelic4@gmail.com>
2021-08-12 12:55:58 +0200

    #31 Added Language dictionary and csv file

---
+++ App/PositionTracking/LanguageDictionary.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using PositionTracking.Data;
+
+namespace PositionTracking
+{
+    public class LanguageDictionary
+    {
+        private readonly string _filePath;
+        private Dictionary<Languages, int> _languageIndexes;
+
+        private Dictionary<string, string[]> _translations;
+
+
+        public LanguageDictionary(string filePath)
+        {
+
+            _filePath = filePath;
+            Load();
+
+                }
+
+
+        private void LoadHeader(string[] values)
+        {
+            for (var i=0; i<values.Length; i++)
+            {
+                var x = Enum.Parse<Languages>(values[i]);
+                _languageIndexes.Add(x, i);
+
+            }
+           
+
+        }
+
+        private void Load()
+        {
+            _languageIndexes = new Dictionary<Languages, int>();
+            _translations = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+
+            using (var reader = new StreamReader(_filePath))
+            {
+                var firstLine = true;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    
+                    if (line == null)
+                        continue;
+
+                    var values = line.Split(",");
+                    if (firstLine)
+                    {
+                        LoadHeader(values);
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        _translations.Add(values[0], values);
+                    }
+                }
+                    
+            }
+
+        }
+        public string Translate(string value, Languages language)
+        {
+            if (language == Languages.en)
+                return value;
+
+            var index = _languageIndexes[language];
+            return _translations[value][index];
+
+
+
+
+
+
+            
+        }
+
+    }
+}
